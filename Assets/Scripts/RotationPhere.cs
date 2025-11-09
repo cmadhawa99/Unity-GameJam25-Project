1ae96b3b9627b01bfd23fd4e11d14ccaf686867f
daser46 <depraba.dp@gmail.com>
2025-11-09 10:51:00 +0530

    test

---
+++ Assets/Scripts/RotationPhere.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RotationPhere : MonoBehaviour
+{
+    public static float rotationSpeed = -50f; 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
